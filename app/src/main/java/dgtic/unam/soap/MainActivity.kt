174ffe8d87ee368a6f71291d58dce4b8c4d3793a
ReusHarper <reusharper@comunidad.unam.mx>
2022-12-04 18:52:52 -0600

    Initial commit

---
+++ app/src/main/java/dgtic/unam/soap/MainActivity.kt
@@ -0,0 +1,41 @@
+package dgtic.unam.soap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import dgtic.unam.soap.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val executor = Executors.newSingleThreadExecutor()
+    private val myHandler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            val dataOne = binding.parameterOne.text.toString().trim()
+            val dataTwo = binding.parameterTwo.text.toString().trim()
+            getService(dataOne, dataTwo)
+        }
+    }
+
+    private fun getService(dataOne: String, dataTwo: String) {
+        executor.execute {
+            val response = CallServices().callApi(Utils.METHOD_ADD, dataOne, dataTwo)
+            myHandler.post {
+                try {
+                    binding.textView.text = response
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+}
